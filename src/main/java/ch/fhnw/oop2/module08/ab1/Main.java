beb3c266b51d6573c8fe25630d1d766626e4cde8
Kunz Tobias (s) <tobias.kunz@students.fhnw.ch>
2020-04-07 08:14:52 +0200

    init commit

---
+++ src/main/java/ch/fhnw/oop2/module08/ab1/Main.java
@@ -0,0 +1,14 @@
+package ch.fhnw.oop2.module08.ab1;
+
+
+public class Main {
+
+	/**
+	 * Starting the application here because of this bug
+	 * https://github.com/javafxports/openjdk-jfx/issues/236
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	//	Starter.main(args);
+	}
+}
---
+++ src/main/java/ch/fhnw/oop2/module08/ab2/Main.java
@@ -0,0 +1,13 @@
+package ch.fhnw.oop2.module08.ab2;
+
+public class Main {
+
+	/**
+	 * Starting the application here because of this bug
+	 * https://github.com/javafxports/openjdk-jfx/issues/236
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	//	Starter.main(args);
+	}
+}
